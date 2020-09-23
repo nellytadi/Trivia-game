a572ff6aa04aabd2f85524656f86dd71335279cd
nellytadi <nellytadi@gmail.com>
2020-09-23 10:20:17 +0100

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::get('/players', [PlayerController::class,'index']);
+Route::get('/players/{id}', [PlayerController::class,'show']);
+Route::post('/players', [PlayerController::class,'store']);
+Route::post('/players/{id}/answers', [PlayerController::class,'answer']);
+Route::delete('/players/{id}', [PlayerController::class,'delete']);
+Route::delete('/players/{id}/answers', [PlayerController::class,'resetAnswers']);
